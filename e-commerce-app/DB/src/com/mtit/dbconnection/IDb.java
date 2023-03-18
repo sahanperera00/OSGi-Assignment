3358cbef9a430595629f171d60344fa918abccd3
Sahan Perera <sahanvimukthi00@gmail.com>
2023-03-18 17:39:44 +0530

    deleted

--- e-commerce-app/DB/src/com/mtit/dbconnection/IDb.java
+++
@@ -1,7 +0,0 @@
-package com.mtit.dbconnection;
-
-import java.sql.Connection;
-
-public interface IDb {
-	Connection connection();
-}
--- e-commerce-app/ServicePublisher/src/com/mtit/service/ServicePublish.java
+++
@@ -1,9 +0,0 @@
-package com.mtit.service;
-
-public interface ServicePublish {
-	public void registerUser();
-	public ServiceModel loginUser();
-	public void viewUser(ServiceModel instance);
-	public void updateUser(ServiceModel instance);
-	public void deleteUser();
-}
